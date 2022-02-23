675f3daa557445da793cc08fb62b2a228cda80cb
Ваня Медведев <ranzar2000@gmail.com>
2022-02-24 01:23:50 +0300

    Завершить скрипт delete

--- delete.php
+++ delete.php
@@ -1,1 +1,17 @@
-<?php require "require_db.php"; ?>
+<?php require "require_db.php"; 
+$id = $_REQUEST['id'];
+$cat = R::load('notes', $id);
+R::trash($cat);
+// $host="localhost";
+// $user="root";
+// $pass="";
+// $db="registration";
+
+// $con = mysqli_connect($host, $user, $pass) or die ("no connection");
+// mysqli_select_db($con, $db) or die ("ni select db");
+// $id = $_REQUEST['id'];
+// $query="DELETE FROM `notes` WHERE `notes`.`id` =".$id;
+// mysqli_query($con, $query);
+header('Location: index.php');
+
+?>
