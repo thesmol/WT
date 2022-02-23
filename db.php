46ba87bfd56d92a7b2280e61a25391532270d0a2
Ваня Медведев <ranzar2000@gmail.com>
2022-02-24 00:05:28 +0300

    Проверить соединение с базой данных

--- db.php
+++ db.php
@@ -3,5 +3,9 @@
 require "libs/rb-mysql.php";
 R::setup( 'mysql:host=localhost;dbname=registration',
 'root', '' );
+if ( !R::testConnection() )
+{
+        exit ('Нет соединения с базой данных');
+}
 
 session_start();
