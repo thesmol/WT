84fb361eac8db6b3aeca6975a36408780b4f731b
Ваня Медведев <ranzar2000@gmail.com>
2022-02-24 00:05:11 +0300

    Добавить заметку и вывести заметки

--- index.php
+++ index.php
@@ -7,13 +7,69 @@
         <title>Document</title>
 </head>
 <?php
+ $data = $_POST;
+ if( isset($data['add_note']))
+ {
+    $errors = array();
+    if( trim($data['note']) == '')
+        {
+            $errors[] = 'Запишите вашу заметку!';
+        }
+        if( R::count('notes', "note = ?", array($data['note'])) > 0)
+        {
+            $errors[] = 'Такая заметка уже существует';
+        }
+    if( empty($errors))
+        {
+            $note = R::dispense('notes');
+            $note -> note = $data['note'];
+            $note -> id_user = $_SESSION['logged_user']->id;
+            R::store($note);
+            echo '<div class="mb-3" style ="color: green;">Ваша заметка сохранена</div><hr>';
+        }
+    else
+        {
+            echo '<div class="mb-3" style ="color: red;">'.array_shift($errors).'</div><hr>';
+        }        
+ }
+?>
+<?php
 if( isset($_SESSION['logged_user']) ): 
 ?>
-    Вы авторизованы
-    <hr>
-    
-    Привет, <?php echo $_SESSION['logged_user']->login; ?>!<br>
-    <a href="logout.php">Выйти</a><br>
+    <form action="index.php" method ="POST">
+        <div class="mb-3">
+        <p>Привет, <?php echo $_SESSION['logged_user']->login; ?>!
+        <a href="logout.php">Выйти</a><br></p>
+        <p>
+            <p><strong>Ваша заметка</strong>:</p>
+            <input type="text" name = "note" class="form-control">
+        </p>
+        <p>
+            <input type="submit" value="Добавить заметку" class="btn btn-primary" name = "add_note"><br>
+        </p>
+        </div>
+    </form>
+    <form action="index.php" method ="POST">
+    <table border=1>
+        <tr>
+            <td> Заметка</td>
+            <td> Удалить </td>
+            <td> Изменить </td>
+        </tr>
+        
+</table> 
+    <?php
+    $array = array(
+        $_SESSION['logged_user']->id,   
+    );
+    $notes = R::find('notes', 'id_user = ?', array($_SESSION['logged_user']->id));
+    foreach($notes as $note)
+    {
+        echo 'Note: '.$note->note. '<br>';
+    }
+    ?>
+</form>
+
 
 <?php else: ?>
         <?php require "login.php"; ?>
