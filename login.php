2c2606438d218fcc705c79676801ee9d2d228adb
Ваня Медведев <ranzar2000@gmail.com>
2022-02-23 21:58:38 +0300

    Перенаправить при входе на индексную страницу

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<?php require "require_db.php"; ?>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
@@ -17,7 +16,8 @@
             {
                 //все норм пора логинить пользователя
                 $_SESSION['logged_user'] = $user; // или всего пользователя или его айди
-                echo '<div class="mb-3" style ="color: green;">Вы успешно авторизованы! Можете перейти на <a href="index.php">Главную </a>страницу</div><hr>';
+                //echo '<div class="mb-3" style ="color: green;">Вы успешно авторизованы!</div><hr>';
+                header('Location: index.php');
             }
             else
             {
@@ -38,7 +38,7 @@
 
     }
 ?>
-<form action="login.php" method ="POST">
+<form action="login2.php" method ="POST">
     <div class="mb-3">
     <p>
         <p><strong>Логин</strong>:</p>
@@ -51,8 +51,8 @@
     </p>
 
     <p>
-        <input type="submit" value="Войти" class="btn btn-primary" name = "do_login">
-        <a href="index.php">Главная</a>
+        <input type="submit" value="Войти" class="btn btn-primary" name = "do_login"><br>
+        <a href="signup.php">Регистрация</a>
     </p>
     </div>
 </form>
