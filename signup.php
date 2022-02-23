3702db026c0176144736a8ab182ecf7730e40ed3
Ваня Медведев <ranzar2000@gmail.com>
2022-02-23 16:24:48 +0300

    Проверить на ошибки введенные данные при рег.

--- signup.php
+++ signup.php
@@ -1,34 +1,73 @@
 <?php
-require "db.php";
+    require "db.php";
+
+    $data = $_POST;
+    if( isset($data['do_sighup']))
+    {
+        $errors = array();
+        // здесь регистрируем
+        if( trim($data['login']) == '')
+        {
+            $errors[] = 'Введите логин!';
+        }
+
+        if( trim($data['email']) == '')
+        {
+            $errors[] = 'Введите почту!';
+        }
+
+        if( $data['password'] == '')
+        {
+            $errors[] = 'Введите пароль!';
+        }
+
+        if( $data['password_2'] != $data['password'])
+        {
+            $errors[] = 'Пароль подтвержден не верно!';
+        }
+
+        if( empty($errors))
+        {
+            // все норм можно регистрировать
+
+        }
+        else
+        {
+            echo '<div class="mb-3" style ="color: red;">'.array_shift($errors).'</div><hr>';
+        }
+    }
+
 ?>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 </head>
 
+
+
 <form action="signup.php" method ="POST">
     <div class="mb-3">
     <p>
         <p><strong>Введите логин</strong>:</p>
-        <input type="text" name = "login" class="form-control">
+        <input type="text" name = "login" class="form-control" required>
     </p>
     <p>
         <p><strong>Введите электронную почту</strong>:</p>
-        <input type="email" name = "email" class="form-control">
+        <input type="email" name = "email" class="form-control" required>
     </p>
 
     <p>
         <p><strong>Создайте пароль</strong>:</p>
-        <input type="password" name = "password" class="form-control">
+        <input type="password" name = "password" class="form-control" required>
     </p>
 
     <p>
         <p><strong>Подтвердите пароль</strong>:</p>
-        <input type="password" name = "password_2" class="form-control">
+        <input type="password" name = "password_2" class="form-control" required>
     </p>
 
     <p>
-        <input type="submit" value="Зарегистрироваться" class="btn btn-primary">
+        <buttom type="submit" class="btn btn-primary" тname = "do_sighup">Зарегистрироваться</buttom>
     </p>
     </div>
 </form>
